b8fa78d6d933cd5008e729b08dfac0a5d47ab170
Michał Ławnikowicz <152413739+eloMichas@users.noreply.github.com>
2024-02-05 16:02:07 +0100

    sortowanie listy.py

--- sortowanie listy.py
+++ sortowanie listy.py
@@ -2,16 +2,16 @@
     Program ma za zadanie posortowanie alfabetycznie wpisanych słów
 '''
 list=[]
-#Pętla nieskończona do pytania odnośnie produktów które potem sie doda
+# Pętla nieskończona do pytania odnośnie produktów które potem sie doda
 while True:
     x=input('(0 żeby zakończyć)Dodaj słówko: ')
     list.append(x)
-#Dzięki tej części, po wpisaniu '0', aby wyjść z pętli. Pętla zostanie przerwana i zostanie usunięty ostatni element, czyli '0'
+# Dzięki tej części, po wpisaniu '0', aby wyjść z pętli. Pętla zostanie przerwana i zostanie usunięty ostatni element, czyli '0'
     if x==str(0):
         list.pop()
         break
-#Sortowanie listy
+# Sortowanie listy
 list.sort()
-#Wypisanie listy od góry do dołu pojedyńczo
+# Wypisanie listy od góry do dołu pojedyńczo
 for w in list:
     print(w)
